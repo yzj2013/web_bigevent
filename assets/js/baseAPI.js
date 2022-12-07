15dbd44c557b93dd931518c67cc9409a847f892d
itheima <itheima@itcast.cn>
2022-12-07 20:32:47 +1100

    completed index

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,6 +3,34 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
   //   在发起真正的Ajax请求之前， 统一拼接请求的根路径
-  options.url = "http://www.liulongbin.top:3007/" + options.url;
-  console.log(options.url);
+  options.url = "http://www.liulongbin.top:3007" + options.url;
+  // console.log(options.url);
+
+  // 统一为有权限的接口，设置headers请求头
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = { Authorization: localStorage.getItem("token") || "" };
+  }
+
+  // 全局统一挂载 complete回调函数
+  options.complete = function (res) {
+    res = {
+      responseJSON: {
+        status: 0,
+        message: "身份认证成功！",
+      },
+    };
+    //   console.log("执行了 complete回调函数：");
+    console.log(res);
+    //   在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      //   1. 强制清空token
+      localStorage.removeItem("token");
+      //   2. 强制跳转到登录页面
+      location.href = "/login.html";
+    }
+  };
 });
---
+++ assets/js/index.js
@@ -0,0 +1,99 @@
+$(function () {
+  // 调用getUserInfo获取用户基本信息
+  getUserInfo();
+
+  let layer = layui.layer;
+
+  // 点击按钮，实现退出功能
+  $("#btnLogout").on("click", function () {
+    //   console.log("ok");
+    // 提示用户是否确定退出
+    layer.confirm(
+      "确定退出登录?",
+      { icon: 3, title: "提示" },
+      function (index) {
+        //do something
+        // console.log("ok");
+        // 1. 清空本地存储的 token
+        localStorage.removeItem("token");
+        // 2. 重新跳转到登录页面
+        location.href = "/login.html";
+
+        // 关闭 confirm 询问框
+        layer.close(index);
+      }
+    );
+  });
+});
+
+// 获取用户的基本信息
+function getUserInfo() {
+  $.ajax({
+    method: "GET",
+    url: "/my/userinfo",
+    // headers就是请求头配置对象
+    // Headers: {
+    //   Authorization: localStorage.getItem("token") || "",
+    // },
+    success: function (res) {
+      res = {
+        status: 0,
+        message: "获取用户基本信息成功",
+        data: {
+          id: 17,
+          username: "bb",
+          nickname: "",
+          email: "",
+          user_pic: null,
+        },
+      };
+      //   console.log(res);
+      if (res.status !== 0) {
+        return layui.layer.msg("获取用户信息失败！");
+      }
+      //   调用 renderAvatar 渲染用户的头像
+      renderAvatar(res.data);
+    },
+
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    // complete: function (res) {
+    //   res = {
+    //     responseJSON: {
+    //       status: 0,
+    //       message: "身份认证成功！",
+    //     },
+    //   };
+    //   //   console.log("执行了 complete回调函数：");
+    //   console.log(res);
+    //   //   在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+
+    //   if (
+    //     res.responseJSON.status === 1 &&
+    //     res.responseJSON.message === "身份认证失败！"
+    //   ) {
+    //     //   1. 强制清空token
+    //     localStorage.removeItem("token");
+    //     //   2. 强制跳转到登录页面
+    //     location.href = "/login.html";
+    //   }
+    // },
+  });
+}
+
+function renderAvatar(user) {
+  // 1. 获取用户的名称
+  let name = user.nickname || user.username;
+  // 2. 设置欢迎的文本
+  $("#welcome").html("Welcome&nbsp;&nbsp;" + name);
+  //   3. 按需渲染用户的头像
+  if (user.user_pic !== null) {
+    // 3.1 渲染图片头像
+    $(".layui-nav-img").attr("src", user.user_pic).show();
+    $(".text-avatar").hide();
+  } else {
+    // 3.2 渲染文本头像
+    $(".layui-nav-img").hide();
+    let first = name[0].toUpperCase();
+    $(".text-avatar").html(first).show();
+  }
+}
